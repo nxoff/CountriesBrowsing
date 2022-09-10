d8b6b97240193f0fa75fa2076a1d81cb7f37b95d
Narcikss <mdgamedeveloper@gmail.com>
2022-09-10 21:56:20 +0200

    add randomly picking countries

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const COUNTRIES_BOX = document.querySelector(".country-box");
-const COUNTRY_FLAG = document.querySelector(".country-box__flag");
+const SECTION_COUNTRIES = document.querySelector(".section--countries");
 
 async function getCountriesApi() {
 	const response = await fetch("https://restcountries.com/v3.1/all");
@@ -8,8 +7,39 @@ async function getCountriesApi() {
 
 getCountriesApi()
 	.then((data) => {
-		COUNTRY_FLAG.src = data[0].flags.png;
+		createArrayWithNumbers(data, 100);
 	})
 	.catch((err) => {
 		console.log(err);
 	});
+
+function createNewCountryObject(path) {
+	const countryBox = document.createElement("div");
+	const countryFlag = document.createElement("img");
+
+	countryBox.classList.add("country-box");
+	countryFlag.classList.add("country-box__flag");
+	countryFlag.src = path;
+
+	countryBox.append(countryFlag);
+	SECTION_COUNTRIES.appendChild(countryBox);
+}
+
+function createArrayWithNumbers(data, max) {
+	var max = 100;
+	var random = [];
+	for (var i = 1; i < max; i++) {
+		var temp = Math.floor(Math.random() * max);
+		if (random.indexOf(temp) == -1) {
+			random.push(temp);
+		} else i--;
+	}
+	randomlyPickCountries(data, random);
+}
+
+function randomlyPickCountries(data, array) {
+	for (let i = 0; i < 12; i++) {
+		let path = data[array[i]].flags.png;
+		createNewCountryObject(path);
+	}
+}

86552efdcc3d1f26b487116b77de201d63db2bfb
Narcikss <mdgamedeveloper@gmail.com>
2022-09-10 23:17:42 +0200

    add country's information and start building country class

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const SECTION_COUNTRIES = document.querySelector(".section--countries");
+const POPULATION_TEXT = "Population: ";
+const REGION_TEXT = "Region: ";
+const CAPITAL_TEXT = "Capital: ";
 
 async function getCountriesApi() {
 	const response = await fetch("https://restcountries.com/v3.1/all");
@@ -7,21 +10,39 @@ async function getCountriesApi() {
 
 getCountriesApi()
 	.then((data) => {
+		console.log(data);
 		createArrayWithNumbers(data, 100);
 	})
 	.catch((err) => {
 		console.log(err);
 	});
 
-function createNewCountryObject(path) {
+function createNewCountryObject(data, path) {
 	const countryBox = document.createElement("div");
+
 	const countryFlag = document.createElement("img");
+	const countryName = document.createElement("h2");
+	const countryPopulation = document.createElement("span");
+	const countryRegion = document.createElement("span");
+	const countryCapital = document.createElement("span");
 
 	countryBox.classList.add("country-box");
 	countryFlag.classList.add("country-box__flag");
+	countryName.classList.add("country-box__name");
+	countryPopulation.classList.add("country-box__information");
+	countryRegion.classList.add("country-box__information");
+	countryCapital.classList.add("country-box__information");
 	countryFlag.src = path;
+	countryName.textContent = data.name.common;
+	countryPopulation.textContent = `${POPULATION_TEXT}${data.population}`;
+	countryRegion.textContent = `${REGION_TEXT}${data.region}`;
+	countryCapital.textContent = `${CAPITAL_TEXT}${data.capital}`;
 
 	countryBox.append(countryFlag);
+	countryBox.append(countryName);
+	countryBox.append(countryPopulation);
+	countryBox.append(countryRegion);
+	countryBox.append(countryCapital);
 	SECTION_COUNTRIES.appendChild(countryBox);
 }
 
@@ -40,6 +61,6 @@ function createArrayWithNumbers(data, max) {
 function randomlyPickCountries(data, array) {
 	for (let i = 0; i < 12; i++) {
 		let path = data[array[i]].flags.png;
-		createNewCountryObject(path);
+		createNewCountryObject(data[array[i]], path);
 	}
 }
---
+++ country.js
@@ -0,0 +1,8 @@
+class Country {
+	constructor(name, population, region, capital) {
+		this.name = name;
+		this.population = population;
+		this.region = region;
+		this.capital = capital;
+	}
+}

1ea9272e8062129e3c344af0d9d9802b8195a42b
Narcikss <mdgamedeveloper@gmail.com>
2022-09-11 22:34:48 +0200

    modified createCountryObject function to another file

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 import { shuffle } from "./shuffle.js";
-
-const SECTION_COUNTRIES = document.querySelector(".section--countries");
-const currentCountries = [];
+import { createCountryObject } from "./countryDOM.js";
 
 async function getCountriesApi() {
 	const response = await fetch("https://restcountries.com/v3.1/all");
@@ -15,34 +13,6 @@ getCountriesApi()
 	.catch((err) => {
 		console.log(err);
 	});
-
-function createCountryObject(data, index) {
-	const countryBox = document.createElement("div");
-	const countryFlag = document.createElement("img");
-	const countryName = document.createElement("h2");
-	const countryPopulation = document.createElement("span");
-	const countryRegion = document.createElement("span");
-	const countryCapital = document.createElement("span");
-	countryBox.classList.add("country-box");
-	countryFlag.classList.add("country-box__flag");
-	countryName.classList.add("country-box__name");
-	countryPopulation.classList.add("country-box__information");
-	countryRegion.classList.add("country-box__information");
-	countryCapital.classList.add("country-box__information");
-
-	countryFlag.src = data[index].flags.png;
-	countryName.textContent = data[index].name.common;
-	countryPopulation.textContent = "Population: " + data[index].population;
-	countryRegion.textContent = "Region: " + data[index].region;
-	countryCapital.textContent = "Capital: " + data[index].capital;
-	countryBox.appendChild(countryFlag);
-	countryBox.appendChild(countryName);
-	countryBox.appendChild(countryPopulation);
-	countryBox.appendChild(countryRegion);
-	countryBox.appendChild(countryCapital);
-
-	SECTION_COUNTRIES.appendChild(countryBox);
-}
 
 function randomlyPickCountries(data) {
 	const randomNumbers = [];
---
+++ countryDOM.js
@@ -0,0 +1,29 @@
+const SECTION_COUNTRIES = document.querySelector(".section--countries");
+
+export function createCountryObject(data, index) {
+	const countryBox = document.createElement("div");
+	const countryFlag = document.createElement("img");
+	const countryName = document.createElement("h2");
+	const countryPopulation = document.createElement("span");
+	const countryRegion = document.createElement("span");
+	const countryCapital = document.createElement("span");
+	countryBox.classList.add("country-box");
+	countryFlag.classList.add("country-box__flag");
+	countryName.classList.add("country-box__name");
+	countryPopulation.classList.add("country-box__information");
+	countryRegion.classList.add("country-box__information");
+	countryCapital.classList.add("country-box__information");
+
+	countryFlag.src = data[index].flags.png;
+	countryName.textContent = data[index].name.common;
+	countryPopulation.textContent = "Population: " + data[index].population;
+	countryRegion.textContent = "Region: " + data[index].region;
+	countryCapital.textContent = "Capital: " + data[index].capital;
+	countryBox.appendChild(countryFlag);
+	countryBox.appendChild(countryName);
+	countryBox.appendChild(countryPopulation);
+	countryBox.appendChild(countryRegion);
+	countryBox.appendChild(countryCapital);
+
+	SECTION_COUNTRIES.appendChild(countryBox);
+}

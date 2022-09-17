7eb6a2ebec67f699ddd44a2e3809acc663f818b4
Narcikss <mdgamedeveloper@gmail.com>
2022-09-17 12:33:58 +0200

    all filters work

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const searchFilterText = document.querySelector(
 	".searching-filter-box__filter"
 );
 
+const selectFilter = document.querySelector(".by-region-filter-box__select");
+
 async function getCountriesApi() {
 	const response = await fetch("https://restcountries.com/v3.1/all");
 	return await response.json();
@@ -14,8 +16,18 @@ async function getCountriesApi() {
 getCountriesApi()
 	.then((data) => {
 		randomlyPickCountries(data);
+
 		searchFilterText.addEventListener("input", (e) => {
 			countriesBySearchingFilter(data);
+		});
+
+		selectFilter.addEventListener("change", (e) => {
+			let selectedOption =
+				selectFilter.options[selectFilter.selectedIndex].text;
+
+			if (selectedOption === "All") showAllCountries(data);
+			else if (selectedOption === "Random") randomlyPickCountries(data);
+			else showCountriesByRegion(data, selectedOption);
 		});
 	})
 	.catch((err) => {
@@ -30,20 +42,42 @@ function randomlyPickCountries(data) {
 
 	let random = shuffle(randomNumbers);
 
+	refreshCountries();
+
 	for (let i = 0; i < 12; i++) {
 		createCountryObject(data, random[i]);
 	}
 }
 
 function countriesBySearchingFilter(data) {
-	const mapped = data;
-	const filtered = mapped.filter((item) =>
+	const filtered = data.filter((item) =>
 		item.name.common
 			.toLowerCase()
-			.includes(searchFilterText.value.toLowerCase())
+			.includes(searchFilterText.value.toLowerCase().trim())
 	);
 
 	refreshCountries();
+
+	for (let i = 0; i < filtered.length; i++) {
+		createCountryObject(filtered, i);
+	}
+}
+
+function showAllCountries(data) {
+	refreshCountries();
+
+	for (let i = 0; i < data.length; i++) {
+		createCountryObject(data, i);
+	}
+}
+
+function showCountriesByRegion(data, region) {
+	const filtered = data.filter((item) =>
+		item.region.toLowerCase().includes(region.toLowerCase())
+	);
+
+	refreshCountries();
+
 	for (let i = 0; i < filtered.length; i++) {
 		createCountryObject(filtered, i);
 	}
